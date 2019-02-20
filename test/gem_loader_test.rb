ca17328edc6b31fe0e49102f44a85887355a2810
himanoa <matsunoappy@gmail.com>
2019-02-21 08:43:49 +0900

    test(fail): Add gem loader test

---
+++ test/gem_loader_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class GemLoaderTest < Minitest::Test
+  def setup
+
+  end
+
+  def test_load_should_be_require_gems
+    raise NotImplementedError
+  end
+
+  def test_parse_should_be_return_gemfile_dsl
+    raise NotImplementedError
+  end
+
+  def test_parse_should_be_raised_GemfileLock_parse_error
+    raise NotImplementedError
+  end
+end
