241364a5a3819056d7ee8179bcf63f22f1421b39
himanoa <matsunoappy@gmail.com>
2019-03-21 02:07:28 +0900

    feat: Implement GemLoader#load

--- lib/touch_erb/gem_loader.rb
+++ lib/touch_erb/gem_loader.rb
@@ -3,5 +3,16 @@ require 'bundler'
 module TouchErb
   class GemLoader
 
+    GEMFILE_NAME = 'Gemfile'
+
+    def initialize(root_dir)
+      @root_dir = root_dir
+    end
+
+    def load
+      builder = Bundler::Dsl.new
+      builder.eval_gemfile(File.join(@root_dir, GEMFILE_NAME))
+      Bundler::Runtime.new(nil, builder.to_definition(nil, true)).setup.require
+    end
   end
 end
